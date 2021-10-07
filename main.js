935c99b327ef17929eeab1b001922f2deba97e1a
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-07 18:36:47 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+JavaScript Object Notation 
+	Its a key value pair 
+	its popular light-weight way of transfering data 
+
+	for example : 
+	Lets try to create a json for Person Object 
+			with name , age , isMarried , gender 
+
+		Its ket value pair 
+		the key is always String and need to be in quotation
+		value can be :
+			String 
+			Number 
+			Boolean 
+			null 
+			array 
+			another json object
+
+
+		This is one json with 5 fields 
+		each key value pair should be separated by comma 
+		{
+			"name" 		: "Anna", 
+			"age" 		: 18  , 
+			"isMarried" : false , 
+			"gender"	: "female", 
+			"company"	: null 
+		}
